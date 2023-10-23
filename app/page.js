9b2afdf83378a58e0fa1befbdc146215adf7bce8
Benjamin Nneji <nnejiben@gmail.com>
2023-10-24 00:25:33 +0100

    Tailwind style
    about me section

--- app/page.js
+++ app/page.js
@@ -7,11 +7,12 @@ import ContactSection from "@/components/ContactSection"
 import Footer from "@/components/Footer"
 import benTall from "../public/images/ben-nneji-tall.webp"
 import HorizontalRule from "@/components/HorizontalRule"
+import ReadMoreBtn from "@/components/ReadMoreBtn"
 
 export default function Home() {
   return (
     <>
-      <section className="home-hero-section h-screen flex flex-col py-6 mb-48">
+      <section className="home-hero-section h-screen flex flex-col py-6 mb-32">
         <div className="mb-6"><HorizontalRule /></div>
         <div className="px-[7.5rem]">
           <h1 className="text-[7rem] text-[#EAEAEA] leading-[90%]">BENJAMIN</h1>
@@ -27,13 +28,11 @@ export default function Home() {
         <div className="mb-8"><HorizontalRule /></div>
       </section>
 
-      <section className="home-about-section section flex mb-52">
+      <section className="home-about-section px-[7.5rem] flex mb-32">
         <div className="flex-1">
-          <h1 className="heading mb-8">HI THERE, BEN HERE</h1>
-          <p className="mb-12">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur.</p>
-          <div className="flex">
-            <Link className="solid-btn" href="/about">READ MORE</Link>
-          </div>
+          <h1 className="text-[4rem] text-[#EAEAEA] leading-[110%] mb-6">HI THERE, BEN HERE</h1>
+          <p className="text-2xl text-[#F9F8F8] mb-8">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur.</p>
+          <Link href="/about"><ReadMoreBtn /></Link>
         </div>
         <div className="flex flex-1 justify-end">
           <div className="home-about-section--img-cont flex">
