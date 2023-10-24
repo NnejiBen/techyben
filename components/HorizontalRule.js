10fe81ba55d02c3d2ad348a046127b85b9d866ba
Benjamin Nneji <nnejiben@gmail.com>
2023-10-24 13:00:55 +0100

    Update HorizontalRule.js

--- components/HorizontalRule.js
+++ components/HorizontalRule.js
@@ -1,7 +1,9 @@
 const HorizontalRule = () => {
   return (
-    <hr className="border-0 h-[2px] bg-[#F9F8F8]"/>
+    <hr className="border-0 h-[2px] bg-[#C0D6DF]"/>
   )
 }
 
 export default HorizontalRule
+
+// Original bg color: #F9F8F8
