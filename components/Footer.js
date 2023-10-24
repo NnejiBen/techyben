dbb90ba07d1db0246ee10f6f6912af48c4708617
Benjamin Nneji <nnejiben@gmail.com>
2023-10-24 12:55:41 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
   return (
-    <footer className="flex justify-between items-center bg-[#020617] px-[7.5rem] py-10">
+    <footer className="flex justify-between items-center bg-[#020617] px-[7.5rem] py-8">
         <div>
             {/* Copyright information */}
             <div className="text-[#EAEAEA] text-[2rem]">
