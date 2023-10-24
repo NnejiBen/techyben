e90dcf274b02cb9854d000f6b63bd16f8fd5fdfd
Benjamin Nneji <nnejiben@gmail.com>
2023-10-24 14:07:17 +0100

    tailwind styling
    complete about section about page

--- app/about/page.js
+++ app/about/page.js
@@ -28,15 +28,18 @@ const about = () => {
       </section>
 
       {/* ABOUT SECTION */}
-      <section className="about-about-section section flex items-center mb-52">
+      <section className="flex items-center px-[7.5rem] mb-52">
         <div className="flex-1">
-          <h2 className="heading mb-14">A LITTLE ABOUT ME</h2>
-          <p className="mb-10">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur.</p>
-          <p>Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur.</p>
+          {/* Heading */}
+          <h2 className="text-[#EAEAEA] text-[4rem] leading-none mb-10">A LITTLE ABOUT ME</h2>
+          {/* Body text */}
+          <p className="text-[#F9F8F8] text-2xl mb-6">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur.</p>
+          <p className="text-[#F9F8F8] text-2xl">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur.</p>
         </div>
         <div className="flex flex-1 justify-end">
-          <div className="about-about-section--img-cont flex">
-            <Image src={benTall} alt="Photo of Ben Nneji standing" className="about-about-section--img object-cover"/>
+          {/* Image */}
+          <div className="flex max-w-[80%] border-8 border-[#8ECAE6] rounded-[272.5px]">
+            <Image src={benTall} alt="Photo of Ben Nneji standing" className="rounded-[272.5px] object-cover"/>
           </div>
         </div>
       </section>
