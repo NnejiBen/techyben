f8ac212eeac6f1a24c03705c4d4688e6c4fee35e
Benjamin Nneji <nnejiben@gmail.com>
2023-10-28 12:40:52 +0100

    responsive portfolio page

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -9,22 +9,27 @@ import Footer from "@/components/Footer"
 import melodyGenie from "@/public/images/melody-genie.webp"
 import electetra from "@/public/images/electetra.webp"
 import jebAutoServices from "@/public/images/jeb-auto-services.webp"
+import MobileNavbar from "@/components/MobileNavbar"
 
 const portfolio = () => {
   return (
     <>
       {/* HERO SECTION */}
-      <section className="py-6 mb-16">
-        <div className="mb-12">
+      <section className="py-6 sm:py-4 mb-16">
+        <div className="mb-12 sm:mb-4">
           <HorizontalRule />
         </div>
-        <div className="px-[7.5rem]">
+        {/* Mobile navbar */}
+        <div className="hidden sm:flex justify-end px-[1.25rem] mb-20">
+          <MobileNavbar />
+        </div>
+        <div className="px-[7.5rem] lg:px-[4rem] md:px-[1.25rem]">
           {/* Title */}
-          <h1 className="text-[#F1F1F1] text-[4rem] leading-none mb-10">MY PORTFOLIO</h1>
-          <div className="flex justify-end mb-4">
-            <p className="w-1/2 text-left text-[#F1F1F1] text-2xl">Here are some of the projects that I've worked on as a frontend software developer. I'm always looking for new challenges and I'm passionate about building innovative and user-friendly websites and web applications. If you have a project in mind, please feel free to contact me.</p>
+          <h1 className="text-[#F1F1F1] text-[4rem] sm:text-[2.5rem] leading-none mb-10 sm:mb-7">MY PORTFOLIO</h1>
+          <div className="flex justify-end mb-4 sm:mb-8">
+            <p className="w-1/2 md:w-2/3 sm:w-full text-left text-[#F1F1F1] text-2xl lg:text-xl sm:text-lg">Here are some of the projects that I've worked on as a frontend software developer. I'm always looking for new challenges and I'm passionate about building innovative and user-friendly websites and web applications. If you have a project in mind, please feel free to contact me.</p>
           </div>
-          <div className="mb-12">
+          <div className="sm:hidden mb-12">
             <Navbar />
           </div>
         </div>
@@ -34,12 +39,12 @@ const portfolio = () => {
       </section>
 
       {/* PROJECTS SECTION */}
-      <section className="px-[7.5rem] mb-32">
+      <section className="px-[7.5rem] lg:px-[4rem] md:px-[1.25rem] mb-32">
 
         {/* Project 01 */}
-        <div className="flex gap-14 items-center mb-16">
+        <div className="flex md:flex-col gap-14 items-center mb-16">
           {/* Left side: Card dots and image */}
-          <div className="min-w-[55%] flex flex-col bg-[#C0D6DF] rounded-3xl">
+          <div className="min-w-[55%] md:min-w-full flex flex-col bg-[#C0D6DF] rounded-3xl">
             {/* Card dots */}
             <div className="flex gap-2 px-8 py-3">
               <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#FFB703]"></div>
@@ -54,8 +59,8 @@ const portfolio = () => {
           </div>
           {/* Right side: Heading, description, and button */}
           <div className="flex flex-col min-w-[40%]">
-            <h2 className="text-5xl text-[#EAEAEA] leading-[110%] mb-6">MELODY GENIE</h2>
-            <p className="text-xl text-[#F9F8F8] mb-8">This website was designed by <Link href="https://hijoygirl.com">Joygirl</Link> and I brought it to life using HTML, CSS, and JavaScript. This is one of my early projects and offered some very interesting challenges such as the animations and music player. It’s a beautiful site and I consider it a work of art.</p>
+            <h2 className="text-5xl xl:text-4xl text-[#EAEAEA] leading-[110%] mb-6">MELODY GENIE</h2>
+            <p className="text-xl sm:text-lg text-[#F9F8F8] mb-8">This website was designed by <Link href="https://hijoygirl.com">Joygirl</Link> and I brought it to life using HTML, CSS, and JavaScript. This is one of my early projects and offered some very interesting challenges such as the animations and music player. It’s a beautiful site and I consider it a work of art.</p>
             <div className="flex">
               <Link href="#"><ReadMoreBtnSolid /></Link>
             </div>
@@ -63,17 +68,17 @@ const portfolio = () => {
         </div>
 
         {/* Project 02 */}
-        <div className="flex gap-14 items-center mb-16">
+        <div className="flex md:flex-col-reverse gap-14 items-center mb-16">
           {/* Left side: Heading, description, and button */}
           <div className="flex flex-col min-w-[40%]">
-            <h2 className="text-5xl text-[#EAEAEA] leading-[110%] mb-6">ELECTETRA</h2>
-            <p className="text-xl text-[#F9F8F8] mb-8">I developed a new website for Electetra, an LED technology company. Built from scratch using HTML, CSS, and JavaScript, this website has a very aesthetically pleasing interface. It features two sliders, a marquee, and other features.</p>
+            <h2 className="text-5xl xl:text-4xl text-[#EAEAEA] leading-[110%] mb-6">ELECTETRA</h2>
+            <p className="text-xl sm:text-lg text-[#F9F8F8] mb-8">I developed a new website for Electetra, an LED technology company. Built from scratch using HTML, CSS, and JavaScript, this website has a very aesthetically pleasing interface. It features two sliders, a marquee, and other features.</p>
             <div className="flex">
               <Link href="#"><ReadMoreBtnSolid /></Link>
             </div>
           </div>
           {/* Right side: Card dots and image */}
-          <div className="min-w-[55%] flex flex-col bg-[#C0D6DF] rounded-3xl">
+          <div className="min-w-[55%] md:min-w-full flex flex-col bg-[#C0D6DF] rounded-3xl">
             {/* Card dots */}
             <div className="flex gap-2 px-8 py-3">
               <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#FFB703]"></div>
@@ -89,9 +94,9 @@ const portfolio = () => {
         </div>
 
         {/* Project 03 */}
-        <div className="flex gap-14 items-center mb-16">
+        <div className="flex md:flex-col gap-14 items-center mb-16">
           {/* Left side: Card dots and image */}
-          <div className="min-w-[55%] flex flex-col bg-[#C0D6DF] rounded-3xl">
+          <div className="min-w-[55%] md:min-w-full flex flex-col bg-[#C0D6DF] rounded-3xl">
             {/* Card dots */}
             <div className="flex gap-2 px-8 py-3">
               <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#FFB703]"></div>
@@ -106,8 +111,8 @@ const portfolio = () => {
           </div>
           {/* Right side: Heading, description, and button */}
           <div className="flex flex-col min-w-[40%]">
-            <h2 className="text-5xl text-[#EAEAEA] leading-[110%] mb-6">JEB AUTO SERVICES</h2>
-            <p className="text-xl text-[#F9F8F8] mb-8">I developed this WordPress website for Jeb Auto Services, an auto repair company. It has a simple and yet beautiful interface. It is responsive and easy to navigate. Some of the salient features include a WhatsApp chat button and contact forms.</p>
+            <h2 className="text-5xl xl:text-4xl text-[#EAEAEA] leading-[110%] mb-6">JEB AUTO SERVICES</h2>
+            <p className="text-xl sm:text-lg text-[#F9F8F8] mb-8">I developed this WordPress website for Jeb Auto Services, an auto repair company. It has a simple and yet beautiful interface. It is responsive and easy to navigate. Some of the salient features include a WhatsApp chat button and contact forms.</p>
             <div className="flex">
               <Link href="#"><ReadMoreBtnSolid /></Link>
             </div>
@@ -118,8 +123,8 @@ const portfolio = () => {
         <div className="hidden gap-14 items-center mb-16">
           {/* Left side: Heading, description, and button */}
           <div className="flex flex-col min-w-[40%]">
-            <h2 className="text-5xl text-[#EAEAEA] leading-[110%] mb-6">PROJECT TITLE</h2>
-            <p className="text-xl text-[#F9F8F8] mb-8">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo.</p>
+            <h2 className="text-5xl xl:text-4xl text-[#EAEAEA] leading-[110%] mb-6">PROJECT TITLE</h2>
+            <p className="text-xl sm:text-lg text-[#F9F8F8] mb-8">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo.</p>
             <div className="flex">
               <Link href="#"><ReadMoreBtnSolid /></Link>
             </div>
