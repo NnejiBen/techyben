a4b51bf7e70defc5d2e42ada8d4f007f13f0dca0
Benjamin Nneji <nnejiben@gmail.com>
2023-10-29 20:27:24 +0100

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -38,7 +38,7 @@ const about = () => {
       {/* Navbar */}
       <div className="sticky top-0 px-[7.5rem] lg:px-[4rem] md:px-[1.25rem] mb-8"><Navbar /></div>
       {/* Horizontal rule */}
-      <div className="mb-32 xl:mb-52 md:mb-24 sm:mb-8"><HorizontalRule /></div>
+      <div className="mb-32 xl:mb-52 md:mb-24 sm:mb-16"><HorizontalRule /></div>
 
       {/* ABOUT SECTION */}
       <section className="flex md:flex-col-reverse items-center px-[7.5rem] lg:px-[4rem] md:px-[1.25rem] mb-52 md:mb-24 sm:mb-16">
