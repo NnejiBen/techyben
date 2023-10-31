0321170c9f84ebf34a4f853938eaa952c8499f67
Benjamin Nneji <nnejiben@gmail.com>
2023-10-31 16:04:49 +0100

    mobile navbar chatgpt

--- components/MobileNavMenu.js
+++ components/MobileNavMenu.js
@@ -1,66 +1,89 @@
-import Link from "next/link"
-import Image from "next/image"
-import githubIcon from "../public/images/github.png"
-import linkedinIcon from "../public/images/linkedin.png"
-import gmailIcon from "../public/images/gmail.png"
-import whatsappIcon from "../public/images/whatsapp.png"
-import xIcon from "../public/images/x.png"
-import facebookIcon from "../public/images/facebook.png"
+'use client'
+
+import Link from "next/link";
+import Image from "next/image";
+import githubIcon from "../public/images/github.png";
+import linkedinIcon from "../public/images/linkedin.png";
+import gmailIcon from "../public/images/gmail.png";
+import whatsappIcon from "../public/images/whatsapp.png";
+import xIcon from "../public/images/x.png";
+import facebookIcon from "../public/images/facebook.png";
 
 const MobileNavMenu = () => {
+  const closeMobileMenu = () => {
+    const mobileMenu = document.querySelector(".mobile-menu");
+    mobileMenu.classList.add("hidden");
+    const menuToggle = document.querySelector("#menuToggle");
+    menuToggle.innerHTML = "<FaBars />";
+  };
+
   return (
     <>
-      <nav className="hidden flex-col justify-between fixed top-0 left-0 w-full h-full bg-[#00162B] text-white px-[1.25rem] pt-32 z-10">
+      <nav className="mobile-menu hidden flex-col justify-between fixed top-0 left-0 w-full h-full bg-[#00162B] text-white px-[1.25rem] pt-32 z-10">
         <div>
-          <div className='border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center'><Link href="/">HOME</Link></div>
-          <div className='border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center'><Link href="/about">ABOUT</Link></div>
-          <div className='border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center'><Link href="/portfolio">PORTFOLIO</Link></div>
-          <div className='border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center'><Link href="/contact">CONTACT</Link></div>
-          <div className='hidden border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center'><Link href="mailto:nnejiben@gmail.com" target="_blank">nnejiben@gmail.com</Link></div>
-          <div className='hidden border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center'><Link href="tel:+2348063062992" target="_blank">234-8063062992</Link></div>
+          <div className="mobile-menu-item border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center">
+            <Link href="/" onClick={closeMobileMenu}>
+              HOME
+            </Link>
+          </div>
+          <div className="mobile-menu-item border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center">
+            <Link href="/about" onClick={closeMobileMenu}>
+              ABOUT
+            </Link>
+          </div>
+          <div className="mobile-menu-item border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center">
+            <Link href="/portfolio" onClick={closeMobileMenu}>
+              PORTFOLIO
+            </Link>
+          </div>
+          <div className="mobile-menu-item border-b-[1px] border-[#EAEAEA] py-8 text-xl text-center">
+            <Link href="/contact" onClick={closeMobileMenu}>
+              CONTACT
+            </Link>
+          </div>
         </div>
         {/* Contact icons */}
-        <div className='flex justify-center gap-4 border-[#EAEAEA] py-8'>
-            {/* Contact icon */}
-            <div>
-                <Link href="mailto:nnejiben@gmail.com" target="_blank">
-                    <Image src={gmailIcon} title="Email" className="max-w-[3.2em] max-h-[3.2em] object-cover"/>
-                </Link>
-            </div>
-            {/* Contact icon */}
-            <div>
-                <Link href="https://api.whatsapp.com/send?phone=2348063062992" target="_blank">
-                    <Image src={whatsappIcon} title="WhatsApp" className="max-w-[3.2em] max-h-[3.2em] object-cover"/>
-                </Link>
-            </div>
-            {/* Contact icon */}
-            <div>
-                <Link href="https://github.com/NnejiBen" target="_blank">
-                    <Image src={githubIcon} title="GitHub" className="max-w-[3.2em] max-h-[3.2em] object-cover"/>
-                </Link>
-            </div>
-            {/* Contact icon */}
-            <div>
-                <Link href="https://www.linkedin.com/in/benjamin-nneji-a4372623a/" target="_blank">
-                    <Image src={linkedinIcon} title="LinkedIn" className="max-w-[3.2em] max-h-[3.2em] object-cover"/>
-                </Link>
-            </div>
-            {/* Contact icon */}
-            <div>
-                <Link href="https://twitter.com/ben_nneji" target="_blank">
-                    <Image src={xIcon} title="X (Twitter)" className="max-w-[3.2em] max-h-[3.2em] object-cover"/>
-                </Link>
-            </div>
-            {/* Contact icon */}
-            <div>
-                <Link href="https://web.facebook.com/techyben" target="_blank">
-                    <Image src={facebookIcon} title="Facebook" className="max-w-[3.2em] max-h-[3.2em] object-cover"/>
-                </Link>
-            </div>
+        <div className="flex justify-center gap-4 border-[#EAEAEA] py-8">
+          {/* Contact icon */}
+          <div>
+            <Link href="mailto:nnejiben@gmail.com" target="_blank">
+              <Image src={gmailIcon} title="Email" className="max-w-[3.2em] max-h-[3.2em] object-cover" />
+            </Link>
+          </div>
+          {/* Contact icon */}
+          <div>
+            <Link href="https://api.whatsapp.com/send?phone=2348063062992" target="_blank">
+              <Image src={whatsappIcon} title="WhatsApp" className="max-w-[3.2em] max-h-[3.2em] object-cover" />
+            </Link>
+          </div>
+          {/* Contact icon */}
+          <div>
+            <Link href="https://github.com/NnejiBen" target="_blank">
+              <Image src={githubIcon} title="GitHub" className="max-w-[3.2em] max-h-[3.2em] object-cover" />
+            </Link>
+          </div>
+          {/* Contact icon */}
+          <div>
+            <Link href="https://www.linkedin.com/in/benjamin-nneji-a4372623a/" target="_blank">
+              <Image src={linkedinIcon} title="LinkedIn" className="max-w-[3.2em] max-h-[3.2em] object-cover" />
+            </Link>
+          </div>
+          {/* Contact icon */}
+          <div>
+            <Link href="https://twitter.com/ben_nneji" target="_blank">
+              <Image src={xIcon} title="X (Twitter)" className="max-w-[3.2em] max-h-[3.2em] object-cover" />
+            </Link>
+          </div>
+          {/* Contact icon */}
+          <div>
+            <Link href="https://web.facebook.com/techyben" target="_blank">
+              <Image src={facebookIcon} title="Facebook" className="max-w-[3.2em] max-h-[3.2em] object-cover" />
+            </Link>
+          </div>
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default MobileNavMenu
+export default MobileNavMenu;
--- components/MobileNavbar.js
+++ components/MobileNavbar.js
@@ -1,28 +1,32 @@
-import Link from "next/link"
-import Image from "next/image"
-import profilePic from "../public/images/ben-nneji.webp"
+'use client'
+
+import Link from "next/link";
+import Image from "next/image";
+import profilePic from "../public/images/ben-nneji.webp";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const MobileNavbar = () => {
+  const toggleMobileMenu = () => {
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const menuToggle = document.querySelector("#menuToggle");
+    mobileMenu.classList.toggle("hidden");
+    menuToggle.innerHTML = mobileMenu.classList.contains("hidden") ? "<FaBars />" : "<FaTimes />";
+  };
+
   return (
     <nav className="flex items-center gap-4 w-max bg-[#F9F8F8] rounded-l-[110px] text-[#023047] text-base font-medium p-[6px]">
-        <div>
-            <div className="max-w-[1.5rem] max-h-[1.5rem] border-2 border-solid border-[#8ECAE6] rounded-full">
-              <Link href="/">
-                <Image src={profilePic} alt="Ben Nneji profile picture" title="Home" className="rounded-full"/>
-              </Link>
-            </div>
+      <div>
+        <div className="max-w-[1.5rem] max-h-[1.5rem] border-2 border-solid border-[#8ECAE6] rounded-full">
+          <Link href="/">
+            <Image src={profilePic} alt="Ben Nneji profile picture" title="Home" className="rounded-full" />
+          </Link>
         </div>
-        {/* <div className="flex flex-col gap-1 mr-1">
-            <div className="w-5 h-[2px] bg-[#1C1B1F]"></div>
-            <div className="w-5 h-[2px] bg-[#1C1B1F]"></div>
-            <div className="w-5 h-[2px] bg-[#1C1B1F]"></div>
-        </div> */}
-        <div>
-          <FaBars />
-        </div>
+      </div>
+      <div id="menuToggle" onClick={toggleMobileMenu}>
+        <FaBars />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default MobileNavbar
+export default MobileNavbar;
