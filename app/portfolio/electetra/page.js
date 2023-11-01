59def6d3d6d5aab0a24f1b0ea8d99ceb226ec655
Benjamin Nneji <nnejiben@gmail.com>
2023-11-01 17:28:50 +0100

    add other project pages

---
+++ app/portfolio/electetra/page.js
@@ -0,0 +1,90 @@
+import Link from "next/link"
+import Image from "next/image"
+import melodyGenie from "@/public/images/melody-genie.webp"
+import HorizontalRule from "@/app/components/HorizontalRule"
+import MobileNavbar from "@/app/components/MobileNavbar"
+import Navbar from "@/app/components/Navbar"
+import ProjectSectionLeftText from "@/app/components/ProjectSectionLeftText"
+import ProjectSectionRightText from "@/app/components/ProjectSectionRightText"
+import ContactSection from "@/app/components/ContactSection"
+
+const Electetra = () => {
+  return (
+    <>
+        {/* Horizontal rule */}
+        <div className="sticky md:bg-[#00162B] sm:bg-[#00162B] bg-opacity-50 pt-6 z-10 sm:pt-4 mb-8 md:mb-0">
+            <HorizontalRule />
+        </div>
+
+        {/* Mobile navbar */}
+        <div className="hidden sticky top-0 md:flex sm:flex justify-end bg-[#00162B] bg-opacity-50 px-[1.25rem] w-full py-4 z-10 md:mb-10">
+            <MobileNavbar />
+        </div>
+
+        {/* HERO SECTION */}
+        <section className="pt-6 mb-6">
+            <div className="px-[7.5rem] lg:px-[4rem] md:px-[1.25rem]">
+                {/* Title */}
+                <h1 className="text-[#F1F1F1] text-[4rem] sm:text-[2.5rem] leading-none mb-10 sm:mb-7">ELECTETRA</h1>
+                {/* Subtitle */}
+                <div className="flex justify-end mb-4 sm:mb-8">
+                    <p className="w-1/2 md:w-2/3 sm:w-full text-left text-[#F1F1F1] text-2xl lg:text-xl sm:text-lg">Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+            </div>
+        </section>
+
+        {/* Navbar */}
+        <div className="sticky top-0 px-[7.5rem] lg:px-[4rem] md:px-[1.25rem] mb-8"><Navbar /></div>
+     
+        {/* Horizontal rule */}
+        <div className="mb-32 xl:mb-52 md:mb-24 sm:mb-16"><HorizontalRule /></div>
+
+        {/* PROJECT SECTION 01*/}
+        <section className="mb-16">
+            <ProjectSectionLeftText 
+                title="HEADING HERE"
+                content="Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur."
+                imgHref={melodyGenie}
+                imgAlt="A short description of the image"
+            />
+
+            <ProjectSectionRightText 
+                title="HEADING HERE"
+                content="Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur."
+                imgHref={melodyGenie}
+                imgAlt="A short description of the image"
+            />
+        </section>
+
+        {/* Full page image */}
+        <div className="flex px-[7.5rem] lg:px-[4rem] md:px-0 mb-16">
+            <Image src={melodyGenie} alt="A short description of the image" className="w-full h-screen sm:h-full object-cover"/>
+        </div>
+
+        {/* PROJECT SECTION 02*/}
+        <section className="mb-32 md:mb-24 sm:mb-14">
+            <ProjectSectionLeftText 
+                title="HEADING HERE"
+                content="Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur."
+                imgHref={melodyGenie}
+                imgAlt="A short description of the image"
+            />
+
+            <ProjectSectionRightText 
+                title="HEADING HERE"
+                content="Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Ut sem mattis condimentum risus risus scelerisque leo. Nibh nec euismod sem pretium. Lorem ipsum dolor sit amet consectetur."
+                imgHref={melodyGenie}
+                imgAlt="A short description of the image"
+            />
+        </section>
+      
+        {/* CONTACT SECTION */}
+        <section className="mb-32 md:mb-24 sm:mb-14">
+            <ContactSection />
+        </section>
+
+    </>
+  )
+}
+
+export default Electetra
