6806e5585f4cac2f88dae584905f2e3a6079bbe4
Benjamin Nneji <nnejiben@gmail.com>
2023-11-04 15:24:45 +0100

    Update PortfolioCard.js

--- app/components/PortfolioCard.js
+++ app/components/PortfolioCard.js
@@ -13,7 +13,7 @@ function PortfolioCard({ number, title, imgSrc, imgAlt, btnLinkHref }) {
                 <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#219EBC]"></div>
                 <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#FB8500]"></div>
               </div>
-              <div className='text-[#FB8500] text-lg md:text-base font-medium flex items-center px-8'>
+              <div className='text-[#219EBC] text-lg md:text-base font-medium flex items-center px-8'>
                 <Link href="#">VIEW PROJECT</Link>
               </div>
           </div>
