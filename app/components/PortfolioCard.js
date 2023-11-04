a9a71bcc770c8be2652dc4335cbf96cdb9cca8d6
Benjamin Nneji <nnejiben@gmail.com>
2023-11-04 14:33:20 +0100

    Update PortfolioCard.js

--- app/components/PortfolioCard.js
+++ app/components/PortfolioCard.js
@@ -14,7 +14,7 @@ function PortfolioCard({ number, title, imgSrc, imgAlt, btnLinkHref }) {
           </div>
           <hr className="border-0 h-[1px] bg-black" />
           {/* Heading and number */}
-          <div className="flex items-center gap-6 px-8 lg:px-4 py-12 lg:py-6 sm:py-8">
+          <div className="flex items-center gap-6 px-8 lg:px-4 py-6 lg:py-6 sm:py-8">
               <div>
                   <p className="text-[#00162B] text-5xl font-bold">{number}</p>
               </div>
@@ -22,11 +22,11 @@ function PortfolioCard({ number, title, imgSrc, imgAlt, btnLinkHref }) {
                   <h3 className="text-[#00162B] text-[2rem] lg:text-[1.5rem] leading-none">{title}</h3>
               </div>
         </div>
-        <div className="w-full mb-10">
+        <div className="w-full mb-6">
               <Image src={imgSrc} alt={imgAlt} className="object-cover" />
         </div>
         {/* Button */}
-        <div className="flex justify-end sm:justify-start px-6 pb-8">
+        <div className="flex justify-end sm:justify-start px-6 pb-6">
               <Link href={btnLinkHref}><ViewProjectBtnOutline /></Link>
         </div>
       </div>
