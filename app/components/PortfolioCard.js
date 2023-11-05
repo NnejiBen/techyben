0d560047fef180da3ac5954d1817e1687872df14
Benjamin Nneji <nnejiben@gmail.com>
2023-11-05 09:56:38 +0100

    update portfolio card

--- app/components/PortfolioCard.js
+++ app/components/PortfolioCard.js
@@ -1,20 +1,16 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import ViewProjectBtnOutline from './ViewProjectBtnOutline';
 
 function PortfolioCard({ number, title, imgSrc, imgAlt, btnLinkHref }) {
   return (
     <>
-      <div className="bg-[#C0D6DF] rounded-3xl">
+      <div className="relative bg-[#C0D6DF] rounded-3xl">
           {/* Card dots */}
           <div className="flex justify-between">
               <div className="flex gap-2 px-8 py-3">
                 <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#FFB703]"></div>
                 <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#219EBC]"></div>
                 <div className="min-w-[24px] min-h-[24px] rounded-full bg-[#FB8500]"></div>
-              </div>
-              <div className='text-[#00162B] hover:text-[#FB8500] text-lg md:text-base font-medium flex items-center px-8'>
-                <Link href={btnLinkHref}>VIEW PROJECT</Link>
               </div>
           </div>
           <hr className="border-0 h-[1px] bg-black" />
@@ -31,8 +27,8 @@ function PortfolioCard({ number, title, imgSrc, imgAlt, btnLinkHref }) {
               <Image src={imgSrc} alt={imgAlt} className="object-cover rounded-b-3xl" />
         </div>
         {/* Button */}
-        <div className="hidden justify-end sm:justify-start px-6 pb-6">
-              <Link href={btnLinkHref}><ViewProjectBtnOutline /></Link>
+        <div className='flex items-center justify-end absolute bottom-0 w-full rounded-b-3xl bg-[#00162B] bg-opacity-50 text-white hover:text-[#FB8500] text-lg md:text-base font-medium px-8 py-3'>
+          <Link href={btnLinkHref}>VIEW PROJECT</Link>
         </div>
       </div>
     </>
