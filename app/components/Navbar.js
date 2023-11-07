c4894954c03bca825305e9d2ef14d3fead0ba23d
Benjamin Nneji <nnejiben@gmail.com>
2023-11-07 20:59:28 +0100

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
   return (
     <Draggable 
       bounds="parent"
-      cancel=".menu-link"
+      cancel=".menu-link .profile-picture"
     >
       <nav title="Click and drag to move" className="md:hidden sm:hidden flex items-center gap-10 w-max bg-[#F9F8F8] rounded-r-[110px] text-[#023047] text-base font-medium p-4 cursor-grab">
         <div className="flex flex-col justify-center ml-6">
@@ -33,7 +33,7 @@ function Navbar() {
         <div>
           <div className="max-w-[6em] border-4 border-solid border-[#8ECAE6] rounded-full">
             <Link href="/">
-              <Image src={profilePic} alt="Ben Nneji profile picture" title="Home" className="rounded-full" />
+              <Image src={profilePic} alt="Ben Nneji profile picture" title="Home" className="profile-picture rounded-full" />
             </Link>
           </div>
         </div>
