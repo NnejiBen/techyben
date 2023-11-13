ea40b41201074714de9fb1236199e461b4cd7d95
Benjamin Nneji <nnejiben@gmail.com>
2023-11-13 13:19:15 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,11 @@
 import './globals.css'
 import MobileNavMenu from '@/app/components/MobileNavMenu'
 import Footer from '@/app/components/Footer'
-import LinkPreview from "./components/LinkPreview"
 
-// export const metadata = {
-//   title: 'Benjamin Nneji | Frontend Web Developer',
-//   description: 'Frontend Web Developer with a love for learning and airplanes. I build modern, elegant and functional websites. I pay great attention to detail.',
-// }
+export const metadata = {
+  title: 'Benjamin Nneji | Frontend Web Developer',
+  description: 'Frontend Web Developer with a love for learning and airplanes. I build modern, elegant and functional websites. I pay great attention to detail.',
+}
 
 export default function RootLayout({ children }) {
   return (
