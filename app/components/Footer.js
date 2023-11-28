775ed2a8b3e1662f3acf44fcfa128ce219adc8fa
Benjamin Nneji <nnejiben@gmail.com>
2023-11-28 16:37:36 +0100

    Update Footer.js
    Update Joygirl link

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
             </div>
             {/* Attribution */}
             <div className="text-[#EAEAEA] text-sm">
-                <p>Designed by <Link href="https://joygirl.super.site/" target="_blank" title="Visit portfolio" className="hover:text-[#FB8500] underline decoration-solid">Joygirl</Link></p>
+                <p>Designed by <Link href="https://hijoygirl.com/" target="_blank" title="Visit portfolio" className="hover:text-[#FB8500] underline decoration-solid">Joygirl</Link></p>
             </div>
         </div>
         <div className='flex gap-6 md:gap-3'>
