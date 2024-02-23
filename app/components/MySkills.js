c840e91183f30bbcddbc0cd8365ec13c9f42fbf0
Benjamin Nneji <nnejiben@gmail.com>
2024-02-23 16:23:20 +0100

    update images to svg

--- app/components/MySkills.js
+++ app/components/MySkills.js
@@ -1,10 +1,10 @@
-import htmlIcon from "@/public/images/html.png"
-import cssIcon from "@/public/images/css.png"
-import javaScriptIcon from "@/public/images/javascript.png"
-import reactIcon from "@/public/images/react.png"
-import nextJsIcon from "@/public/images/next-js.png"
-import tailwindIcon from "@/public/images/tailwind.png"
-import wordPressIcon from "@/public/images/wordpress.png"
+import htmlIcon from "@/public/images/html.svg"
+import cssIcon from "@/public/images/css.svg"
+import javaScriptIcon from "@/public/images/javascript.svg"
+import reactIcon from "@/public/images/react.svg"
+import nextJsIcon from "@/public/images/next-js.svg"
+import tailwindIcon from "@/public/images/tailwind.svg"
+import wordPressIcon from "@/public/images/wordpress.svg"
 import MySkillItem from './MySkillItem'
 
 const MySkills = () => {
