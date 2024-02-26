d11e06851d71f2026fce43d787db25994dcb8ca4
Benjamin Nneji <nnejiben@gmail.com>
2024-02-26 12:03:01 +0100

    Update page.js

--- app/portfolio/deekes-construction/page.js
+++ app/portfolio/deekes-construction/page.js
@@ -29,7 +29,7 @@ const ProjectName = () => {
         <section className="pt-6 mb-6">
             <div className="px-[7.5rem] lg:px-[4rem] md:px-[1.25rem]">
                 {/* Title */}
-                <h1 className="text-[#F1F1F1] text-[4rem] sm:text-[2.5rem] leading-none mb-10 sm:mb-7">DEEKES CONSTRUCTION</h1>
+                <h1 className="text-[#F1F1F1] text-[4rem] sm:text-[2.5rem] leading-none mb-10 sm:mb-7">DEEKES</h1>
                 {/* Subtitle */}
                 <div className="flex justify-end mb-4 sm:mb-8">
                     <p className="w-1/2 md:w-2/3 sm:w-full text-left text-[#F1F1F1] text-2xl lg:text-xl sm:text-lg">I created this WordPress website for Deekes Construction and Real Estate Ltd. The site features a visually appealing interface, intuitive navigation, and full responsiveness.</p>
