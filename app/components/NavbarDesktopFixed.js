083409fff609c8855c1d010f0012550a6d0db369
Benjamin Nneji <nnejiben@gmail.com>
2024-02-27 12:39:46 +0100

    update fixed desktop navbar

--- app/components/NavbarDesktopFixed.js
+++ app/components/NavbarDesktopFixed.js
@@ -1,28 +1,29 @@
 'use client'
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react'; // Import useEffect and useState hooks
 
 function NavbarDesktopFixed() {
+  const [isScrolled, setIsScrolled] = useState(false);
   const currentRoute = usePathname();
-  const [isVisible, setIsVisible] = useState(false); // State to manage navbar visibility
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-      const isVisible = scrollTop > 0; // Set visibility based on scroll position
-      setIsVisible(isVisible);
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
     };
 
-    window.addEventListener('scroll', handleScroll); // Add scroll event listener
-
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll); // Remove scroll event listener on component unmount
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, []); // Empty dependency array to ensure the effect only runs once
+  }, []);
 
   return (
-    <nav className={`md:hidden fixed w-full bg-[#1C1140] bg-opacity-90 z-20 ${isVisible ? 'visible' : 'hidden'}`}>
+    <nav className={`md:hidden fixed w-full bg-[#1C1140] bg-opacity-90 z-20 ${isScrolled ? 'visible' : 'invisible'}`}>
       <div className="section-px flex justify-end gap-4 text-[#F9F8F8] text-base font-normal py-2">
         {/* Home */}
         <Link href="/" className={currentRoute === '/' ? 'active-link-desktop-fixed' : 'menu-link'}>
