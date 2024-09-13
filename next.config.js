6123b5fd9b7cc42d8170c0b808e85eb8ecf465a8
Benjamin Nneji <nnejiben@gmail.com>
2024-09-13 12:03:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const securityHeaders = [
+    {
+      key: 'Content-Security-Policy',
+      value: `default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none';`, // Customize based on your needs
+    },
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff',
+    },
+    {
+      key: 'X-Frame-Options',
+      value: 'DENY',
+    },
+  ];
+  
+  const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
